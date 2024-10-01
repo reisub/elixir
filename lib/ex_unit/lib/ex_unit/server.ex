5c5679f2b683d66ac1dbd1c78446108fc546ab72
Dino Kovač <dino@latinum.xyz>
2024-10-01 21:47:12 +0200

    WIP

--- lib/ex_unit/lib/ex_unit/server.ex
+++ lib/ex_unit/lib/ex_unit/server.ex
@@ -9,7 +9,9 @@ defmodule ExUnit.Server do
     GenServer.start_link(__MODULE__, :ok, name: @name)
   end
 
-  def add_module(name, {async?, parameterize}) do
+  def add_module(name, {async, parameterize}) do
+    dbg({:add_module, name, %{async: async, parameterize: parameterize}})
+
     modules =
       if parameterize do
         Enum.map(parameterize, &{name, &1})
@@ -17,7 +19,7 @@ defmodule ExUnit.Server do
         [{name, %{}}]
       end
 
-    case GenServer.call(@name, {:add, async?, modules}, @timeout) do
+    case GenServer.call(@name, {:add, async, modules}, @timeout) do
       :ok ->
         :ok
 
@@ -51,6 +53,7 @@ defmodule ExUnit.Server do
     state = %{
       loaded: System.monotonic_time(),
       waiting: nil,
+      async_module_groups: %{},
       async_modules: :queue.new(),
       sync_modules: :queue.new()
     }
@@ -91,12 +94,19 @@ defmodule ExUnit.Server do
       when is_integer(loaded) do
     state =
       if uniq? do
-        async_modules = :queue.to_list(state.async_modules) |> Enum.uniq() |> :queue.from_list()
+        async_module_groups =
+          state.async_module_groups
+          |> Enum.map(fn {group_name, q} ->
+            async_modules = :queue.to_list(q) |> Enum.uniq() |> :queue.from_list()
+            {group_name, async_modules}
+          end)
+          |> Map.new()
+
         sync_modules = :queue.to_list(state.sync_modules) |> Enum.uniq() |> :queue.from_list()
 
         %{
           state
-          | async_modules: async_modules,
+          | async_module_groups: async_module_groups,
             sync_modules: sync_modules
         }
       else
@@ -107,13 +117,33 @@ defmodule ExUnit.Server do
     {:reply, diff, take_modules(%{state | loaded: :done})}
   end
 
-  def handle_call({:add, true, names}, _from, %{loaded: loaded} = state)
-      when is_integer(loaded) do
+  def handle_call({:add, async, names}, _from, %{loaded: loaded} = state)
+      when is_integer(loaded) and async not in [false, nil] do
     state =
-      update_in(
-        state.async_modules,
-        &Enum.reduce(names, &1, fn name, q -> :queue.in(name, q) end)
-      )
+      Enum.reduce(names, state, fn name, updated_state ->
+        async_group =
+          case async do
+            true -> default_async_group(name)
+            [{:group, group_name}] -> group_name
+            other -> raise "async can be true or [group: <group_name>], got: #{inspect(other)}"
+          end
+
+        updated_state
+        |> update_in([:async_module_groups], fn async_module_groups ->
+          {_, async_module_groups} =
+            Map.get_and_update(async_module_groups, async_group, fn
+              nil ->
+                {nil, :queue.from_list([name])}
+
+              q ->
+                {q, :queue.in(name, q)}
+            end)
+
+          async_module_groups
+        end)
+        |> update_in([:async_modules], fn q -> :queue.in(async_group, q) end)
+      end)
+      |> dbg()
 
     {:reply, :ok, take_modules(state)}
   end
@@ -128,6 +158,14 @@ defmodule ExUnit.Server do
 
   def handle_call({:add, _async?, _names}, _from, state),
     do: {:reply, :already_running, state}
+
+  defp default_async_group({module, %{}}) do
+    module
+  end
+
+  defp default_async_group({module, params}) do
+    "#{module}_#{:erlang.phash2(params)}"
+  end
 
   defp take_modules(%{waiting: nil} = state) do
     state
